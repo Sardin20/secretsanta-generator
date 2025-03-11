ba5459952f6cce859f0c3293c40a089da7a21245
Sardin20 <107612667+Sardin20@users.noreply.github.com>
2025-03-11 19:16:24 +0530

    Update SecretsantaApplication.java

--- src/main/java/com/notthebest/demo/SecretsantaApplication.java
+++ src/main/java/com/notthebest/demo/SecretsantaApplication.java
@@ -2,12 +2,12 @@ package com.notthebest.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SecretsantaApplication {
+public class SecretsantaApplication extends SpringBootServletInitializer {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SecretsantaApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(SecretsantaApplication.class, args);
+    }
 }
